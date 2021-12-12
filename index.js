f388d86a25bb7ffc247ff21b1c847a75b96e8360
tluwaniMS <tluwani2020@gmail.com>
2021-12-12 15:15:06 +0200

    final-steps-of-server-configuration-and-adding-sample-data

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./main-graph-schema");
 const app = express();
+
+app.use("/graphql", graphqlHTTP({ schema: schema, graphiql: true }));
 
 module.exports = { app };
